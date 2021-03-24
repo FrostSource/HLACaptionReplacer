4ae5aa53b509c87a6742e46f595afd26f9d9de80
FrostSource <jjhfrost@gmail.com>
2021-03-25 02:24:35 +1300

    Renamed HLACaptionReplacer.

--- HLACaptionReplacer/Properties/Resources.Designer.cs
+++ HLACaptionReplacer/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace HLACaptionReplacer.Properties {
+namespace HLACaptionCompiler.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace HLACaptionReplacer.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("HLACaptionReplacer.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("HLACaptionCompiler.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
